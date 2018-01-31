b4a5851a1f1edf5a8e5748295d3b0b2311e43355
akaruitori <dsabh@ya.ru>
2018-01-31 20:44:03 +0300

    Add draft for main.rb

---
+++ main.rb
@@ -0,0 +1,17 @@
+# greet user
+# ask user's name
+# create player for user
+# create dealer
+# create deck
+# loop until true
+#   shuffle deck
+#   deal cards 
+#   show cards
+#   make stakes
+#   loop until both players have 3 cards or 
+#     user choose his move (exit loop if user wish to open his cards)
+#     dealer choose his move
+#   show result
+#   game bank goes to winner
+#   ask if user wish to play another game if players still have at least 10$
+# end game
